e666a91684e774521ed3d5f20b09b31db0901c14
Trim Bresilla <trim.bresilla@gmail.com>
2026-01-02 12:34:23 +0100

    refactor: convert linear_allocator.cpp to header-only

--- include/flywheel/circle.h
+++ include/flywheel/circle.h
@@ -64,4 +64,86 @@ inline int32 Circle::GetSupport(const Vec2& localDir) const
     return 0;
 }
 
+inline Circle::Circle(float radius, const Transform& tf)
+    : Shape(circle, radius)
+{
+    area = radius * radius * pi;
+    center = tf.position;
+}
+
+inline void Circle::ComputeMass(float density, MassData* outMassData) const
+{
+    outMassData->mass = density * area;
+    float inertia = 0.5f * radius * radius;
+    outMassData->inertia = outMassData->mass * (inertia + Length2(center));
+    outMassData->centerOfMass = center;
+}
+
+inline void Circle::ComputeAABB(const Transform& transform, AABB* outAABB) const
+{
+    Vec2 p = Mul(transform, center);
+
+    outAABB->min = Vec2{ p.x - radius, p.y - radius };
+    outAABB->max = Vec2{ p.x + radius, p.y + radius };
+}
+
+inline bool Circle::TestPoint(const Transform& transform, const Vec2& q) const
+{
+    Vec2 localQ = MulT(transform, q);
+    Vec2 d = center - localQ;
+
+    return Dot(d, d) <= radius * radius;
+}
+
+inline Vec2 Circle::GetClosestPoint(const Transform& transform, const Vec2& q) const
+{
+    Vec2 position = Mul(transform, center);
+    Vec2 dir = (q - position);
+
+    float distance = dir.Normalize();
+    if (distance <= radius)
+    {
+        return q;
+    }
+    else
+    {
+        return position + dir * radius;
+    }
+}
+
+inline bool Circle::RayCast(const Transform& transform, const RayCastInput& input, RayCastOutput* output) const
+{
+    Vec2 position = Mul(transform, center);
+
+    Vec2 d = input.to - input.from;
+    Vec2 f = input.from - position;
+
+    float radii = radius + input.radius;
+
+    float a = Dot(d, d);
+    float b = 2.0f * Dot(f, d);
+    float c = Dot(f, f) - radii * radii;
+
+    // Quadratic equation discriminant
+    float discriminant = b * b - 4.0f * a * c;
+
+    if (discriminant < 0.0f)
+    {
+        return false;
+    }
+
+    discriminant = Sqrt(discriminant);
+
+    float t = (-b - discriminant) / (2.0f * a);
+    if (0.0f <= t && t <= input.maxFraction)
+    {
+        output->fraction = t;
+        output->normal = Normalize(f + d * t);
+
+        return true;
+    }
+
+    return false;
+}
+
 } // namespace flywheel
