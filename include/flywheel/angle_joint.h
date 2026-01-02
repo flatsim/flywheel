9ae70ab0da4569edf9645a7ce42ea3d04bcfa0ef
Trim Bresilla <trim.bresilla@gmail.com>
2026-01-02 15:27:28 +0100

    refactor: convert angle_joint.cpp to header-only

--- include/flywheel/angle_joint.h
+++ include/flywheel/angle_joint.h
@@ -10,7 +10,7 @@ namespace flywheel
 class AngleJoint : public Joint
 {
 public:
-    AngleJoint(
+    inline AngleJoint(
         RigidBody* bodyA,
         RigidBody* bodyB,
         float angleOffset,
@@ -21,8 +21,8 @@ public:
         float jointMass
     );
 
-    virtual void Prepare(const Timestep& step) override;
-    virtual void SolveVelocityConstraints(const Timestep& step) override;
+    virtual inline void Prepare(const Timestep& step) override;
+    virtual inline void SolveVelocityConstraints(const Timestep& step) override;
 
     float GetJointAngleOffset() const;
 
@@ -43,8 +43,110 @@ private:
     Vec2 bias;
     Vec2 impulseSum;
 
-    void ApplyImpulse(float lambda);
+    inline void ApplyImpulse(float lambda);
 };
+
+inline AngleJoint::AngleJoint(
+    RigidBody* bodyA,
+    RigidBody* bodyB,
+    float jointAngleOffset,
+    float jointMinAngle,
+    float jointMaxAngle,
+    float jointFrequency,
+    float jointDampingRatio,
+    float jointMass
+)
+    : Joint(angle_joint, bodyA, bodyB, jointFrequency, jointDampingRatio, jointMass)
+    , angleOffset{ jointAngleOffset }
+    , minAngle{ jointMinAngle }
+    , maxAngle{ jointMaxAngle }
+    , impulseSum{ 0.0f }
+{
+    maxAngle = Max(minAngle, maxAngle);
+}
+
+inline void AngleJoint::Prepare(const Timestep& step)
+{
+    ComputeBetaAndGamma(step);
+
+    // Compute Jacobian J and effective mass W
+    // J = [0 -1 0 1]
+    // W = (J · M^-1 · J^t)^-1
+
+    float k = bodyA->invInertia + bodyB->invInertia + gamma;
+
+    if (k != 0.0f)
+    {
+        m = 1.0f / k;
+    }
+
+    Vec2 error(bodyB->motion.a - bodyA->motion.a - angleOffset);
+    error[0] -= minAngle;
+    error[1] -= maxAngle;
+
+    bias = error * beta * step.inv_dt;
+
+    if (step.warm_starting)
+    {
+        if (minAngle == maxAngle)
+        {
+            ApplyImpulse(impulseSum[0]);
+        }
+        else
+        {
+            if (bias[0] < 0)
+            {
+                ApplyImpulse(impulseSum[0]);
+            }
+            if (bias[1] > 0)
+            {
+                ApplyImpulse(impulseSum[1]);
+            }
+        }
+    }
+}
+
+inline void AngleJoint::SolveVelocityConstraints(const Timestep& step)
+{
+    MuliNotUsed(step);
+
+    // Compute corrective impulse: Pc
+    // Pc = J^t · λ (λ: lagrangian multiplier)
+    // λ = (J · M^-1 · J^t)^-1 ⋅ -(J·v+b)
+
+    float jv = bodyB->angularVelocity - bodyA->angularVelocity;
+
+    if (minAngle == maxAngle)
+    {
+        float lambda = m * -(jv + bias[0] + impulseSum[0] * gamma);
+        ApplyImpulse(lambda);
+        impulseSum[0] += lambda;
+    }
+    else
+    {
+        if (bias[0] < 0)
+        {
+            float lambda = m * -(jv + bias[0] + impulseSum[0] * gamma);
+            ApplyImpulse(lambda);
+            impulseSum[0] += lambda;
+        }
+        if (bias[1] > 0)
+        {
+            float lambda = m * -(jv + bias[1] + impulseSum[1] * gamma);
+            ApplyImpulse(lambda);
+            impulseSum[1] += lambda;
+        }
+    }
+}
+
+inline void AngleJoint::ApplyImpulse(float lambda)
+{
+    // V2 = V2' + M^-1 ⋅ Pc
+    // Pc = J^t ⋅ λ
+
+    bodyA->angularVelocity -= lambda * bodyA->invInertia;
+    bodyB->angularVelocity += lambda * bodyB->invInertia;
+}
 
 inline float AngleJoint::GetJointAngleOffset() const
 {
